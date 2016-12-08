8c3c366d3de888a9095e0cc2b7a4fe127c4489b0
talesm <tales.miranda84@gmail.com>
2016-12-08 18:25:40 -0200

    Arp0 refactored as a class.

--- arp0.js
+++ arp0.js
@@ -15,89 +15,97 @@ class Context {
   createDerived() {
     return new Context(this);
   }
-}
 
-function arp0evalElement(ast, context) {
-  context = context || new Context();
-  if(Array.isArray(ast)){
-    return arp0evalElement(ast[0], context).apply(null, ast.slice(1));
-  }
-  switch (ast) {
-    case 'T!': return true;
-    case 'F!': return false;
-    case 'symbol!': return symb => symb;
-    case 'literal!': return function () {
-      return [...arguments]
-    };
-    case 'assign!':return (name, param)=> assign(name, param, context);
-    case 'let!':
-    case 'let-mut!': return (name, param)=>{
-      return context.bindings[name] = arp0evalElement(param, context);
-    };
-    case 'decide!': return (test, trueClause, falseClause) => {
-      const chosenClause = arp0evalElement(test, context)? trueClause: falseClause;
-      return arp0evalElement(chosenClause, context);
-    }
-    case 'do!': return function () {
-      return arp0eval([...arguments], context.createDerived());
-    }
-    case 'loop!': return (clause) => {
-      try {
-        for(;;){
-          arp0evalElement(clause, context);
-        }
-      } catch (e){
-        if(e instanceof LoopBreak) {
-          return e.value;
-        }
-        throw e;
+  get(symbol) {
+    const value = this.bindings[symbol];
+    if(typeof value === 'undefined'){
+      if(!this.base){
+        throw new Error("Symbol '"+symbol+"' not found");
       }
-    };
-    case 'break!': return (value) => {
-      throw new LoopBreak(arp0evalElement(value, context));
-    };
-    case 'macro!': return (paramName, clause) => {
-      return function(){
-        context.bindings[paramName] = [...arguments];
-        return arp0evalElement(clause, context);
+      return this.base.get(symbol);
+    }
+    return value;
+  }
+
+
+  assign(symbol, param) {
+    const oldValue = this.bindings[symbol];
+    if(typeof oldValue === 'undefined'){
+      if(!this.base){
+        throw new Error("Symbol '"+symbol+"' not found");
+      }
+      return this.base.assign(symbol, param, this.base);
+    } else {
+      return this.bindings[symbol] = param;
+    }
+  }
+}
+
+class Arp0{
+  constructor(context){
+    this.context = context = context || new Context();
+  }
+
+  evalElement(ast){
+    if(Array.isArray(ast)){
+      return this.evalElement(ast[0], context).apply(this, ast.slice(1));
+    }
+    switch (ast) {
+      case 'T!': return true;
+      case 'F!': return false;
+      case 'symbol!': return symb => symb;
+      case 'literal!': return function () {
+        return [...arguments]
       };
-    };
-    default:
-      return getBindedValue(ast, context);
+      case 'let!':
+      case 'let-mut!': return (name, param)=>{
+        return this.context.bindings[name] = this.evalElement(param);
+      };
+      case 'assign!':return (name, param)=> this.context.assign(name, this.evalElement(param));
+      case 'decide!': return (test, trueClause, falseClause) => {
+        const chosenClause = this.evalElement(test, context)? trueClause: falseClause;
+        return this.evalElement(chosenClause, context);
+      }
+      case 'do!': return function () {
+        const derived = new Arp0(this.context.createDerived());
+        return derived.eval([...arguments]);
+      }
+      case 'loop!': return (clause) => {
+        try {
+          for(;;){
+            this.evalElement(clause, context);
+          }
+        } catch (e){
+          if(e instanceof LoopBreak) {
+            return e.value;
+          }
+          throw e;
+        }
+      };
+      case 'break!': return (value) => {
+        throw new LoopBreak(this.evalElement(value, context));
+      };
+      case 'macro!': return (paramName, clause) => {
+        return function(){
+          const derived = new Arp0(this.context.createDerived());
+          derived.context.bindings[paramName] = [...arguments];
+          return derived.evalElement(clause, context);
+        };
+      };
+      default:
+        return this.context.get(ast);
+    }
   }
-}
 
-function assign(symbol, param, context) {
-  if(!context){
-    throw new Error("Symbol '"+symbol+"' not found");
+  eval(ast, context) {
+    return ast.reduce((prev, statement) => {
+      return this.evalElement(statement);
+    }, null);
   }
-  const oldValue = context.bindings[symbol];
-  if(typeof oldValue !== 'undefined'){
-    return context.bindings[symbol] = arp0evalElement(param, context);
-  } else {
-    return assign(symbol, param, context.base);
-  }
-}
-
-function getBindedValue(symbol, context) {
-  if(!context){
-    throw new Error("Symbol '"+symbol+"' not found");
-  }
-  const value = context.bindings[symbol];
-  return (typeof value !== 'undefined'
-    ? context.bindings[symbol]
-    : getBindedValue(symbol, context.base)
-  );
-}
-
-function arp0eval(ast, context) {
-  context = context || new Context;
-  return ast.reduce((prev, statement) => {
-    return arp0evalElement(statement, context);
-  }, null);
 }
 
 export default function(input) {
   const ast = parse(input);
-  return arp0eval(ast);
+  const arp = new Arp0();
+  return arp.eval(ast);
 }
